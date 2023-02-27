4c2ace04f65f2b0e78de23f41774a924ca51ae0a
Chanaphat <chanaphat@iapp.co.th>
2023-02-27 11:37:50 +0700

    .

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -1,4 +1,6 @@
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+
 //เข้ารหัส
 export const encryptPassword= async (password:string)=>{
     const salt =await bcrypt.genSalt(Number(process.env.SALT));
@@ -10,4 +12,22 @@ export const encryptPassword= async (password:string)=>{
 export const checkPassword = async (password:string,passwordDB:string)=>{
     const isValid = await bcrypt.compare(password,passwordDB);
     return isValid;
+};
+
+export const generateAccessToken = async (userId:string) => {
+    const token = jwt.sign(
+        { _id: userId },
+    process.env.JWT_ACCESS_SECRET as string,
+    { expiresIn: "30m" }
+    );
+    return token;
+};
+
+export const generateRefreshToken = async (userId:string) => {
+    const token = jwt.sign(
+        { _id: userId },
+    process.env.JWT_REFRESH_TOKEN_SECRET as string,
+    { expiresIn: "1d" }
+    );
+    return token;
 };
