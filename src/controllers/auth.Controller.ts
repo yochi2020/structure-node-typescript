3fd51eeac2a75974bc411f7f275a860b84a3b751
Chanaphat <chanaphat@iapp.co.th>
2023-02-27 11:05:02 +0700

    .

--- src/controllers/auth.Controller.ts
+++ src/controllers/auth.Controller.ts
@@ -1,15 +1,58 @@
-import { Request, Response } from "express";
-import { Result} from "@common/index";
-export const registerWithEmailPasswordController = (req: Request, res: Response) => {
-    Result(res,"");
+import { Request, Response,NextFunction } from "express";
+import { Result,errorHandle } from "@common/index";
+import { UserModel } from "@models/index";
+export const registerWithEmailPasswordController = async (req: Request, res: Response,next:NextFunction) => {
+    try {
+        
+        const { firstname,email,password,password_confirm} = req.body;
+        const userExists = await UserModel.findOne({
+            $or: [{ email: email }, { firstname:firstname }],
+        });
+    
+        if (userExists) {
+            throw errorHandle("username with the same email already exist");
+        }
+
+        if (password !== password_confirm) {
+            throw errorHandle("Password's do not match");
+        }
+        const newUser = await UserModel.create(req.body);
+        // const token = jwt.sign(
+        //     { email, password, username },
+        //     String(process.env.JWT_ACCOUNT_ACTIVATION),
+        //     { expiresIn: "10m" },
+        // );
+
+
+        Result(res,newUser);
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const loginWithEmailPasswordController = (req: Request, res: Response) => {
+export const loginWithEmailPasswordController = async(req: Request, res: Response,next:NextFunction) => {
+    try {
+        // const { firstname,lastname,email,mobile,password,password_confirm} = req.body;
+        
+        // const findUser = await UserModel.findOne({ email });
+        // console.log("🚀 ~ file: auth.Controller.ts:39 ~ loginWithEmailPasswordController ~ findUser:", await findUser?.isPasswordMatched(password));
+        // console.log(await new UserModel().isPasswordMatched(password));
     
-    res.cookie("tokenChi", "eieiei", {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000// 1day
-    });
+       
 
-    Result(res,"test");
+        // if (password !== password_confirm) {
+        //     throw errorHandle("Password's do not match");
+        // }
+
+        // const token = jwt.sign(
+        //     { email, password, username },
+        //     String(process.env.JWT_ACCOUNT_ACTIVATION),
+        //     { expiresIn: "10m" },
+        // );
+
+
+        Result(res,req.body);
+    } catch (error) {
+        next(error);
+    }
 };
---
+++ src/models/user.Model.ts
@@ -0,0 +1,79 @@
+import { encryptPassword,checkPassword } from "@common/index";
+import mongoose,{ ObjectId} from "mongoose";
+
+
+
+export interface IUser {
+    firstname:string
+    lastname:string
+    email:string
+    mobile:string
+    password: string
+    role:string
+}
+
+
+export interface IUserModel extends IUser, Document{
+    isPasswordMatched:(password:string)=>Promise<any>
+}
+
+const userSchema = new mongoose.Schema ({
+    firstname: {
+        type: String,
+        reqired: true,
+        unique: true,
+        index: true,
+    },
+    lastname:{
+        type: String,
+        reqired: true,
+        unique: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        reqired: true,
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        reqired: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        reqired: true,
+    },
+    role: {
+        type: String,
+        default:"user"
+    },
+    cart: {
+        type: Array,
+        default:[]
+    },
+    address: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Address"
+        }]
+    },
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    }]
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    const salt = await encryptPassword(String(this.password));
+    this.password = salt;
+    next();
+});
+
+userSchema.methods = {
+    isPasswordMatched:async function(enteredPassword:string){
+        return await checkPassword(enteredPassword,this.password);
+    }
+};
+
+export const UserModel=  mongoose.model("user", userSchema);
