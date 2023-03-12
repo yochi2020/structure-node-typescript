68a23d66bff11d5a006641e7cdc8a5a8d38c589e
Chanaphat <chanaphat@iapp.co.th>
2023-03-12 16:08:08 +0700

    .

--- src/controllers/auth.Controller.ts
+++ src/controllers/auth.Controller.ts
@@ -1,15 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response,NextFunction } from "express";
 import { Result} from "@common/index";
-export const registerWithEmailPasswordController = (req: Request, res: Response) => {
-    Result(res,"");
+export const registerWithEmailPasswordController = (req: Request, res: Response,next:NextFunction) => {
+    try {
+        Result(res,"");
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const loginWithEmailPasswordController = (req: Request, res: Response) => {
-    
-    res.cookie("tokenChi", "eieiei", {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000// 1day
-    });
-
-    Result(res,"test");
+export const loginWithEmailPasswordController = (req: Request, res: Response,next:NextFunction) => {
+    try {
+        res.cookie("tokenChi", "eieiei", {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000// 1day
+        });
+        Result(res,"test");
+    } catch (error) {
+        next(error);
+    }
 };
