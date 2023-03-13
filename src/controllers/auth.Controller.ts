2494dc30078a3e6637192da3b6e771e29eff136d
Chanaphat <chanaphat@iapp.co.th>
2023-03-13 08:30:42 +0700

    .

--- src/controllers/auth.Controller.ts
+++ src/controllers/auth.Controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response,NextFunction } from "express";
 import { Result } from "@common/index";
-import UserModel from '@models/user.Model'
-import jwt  from 'jsonwebtoken'
+import UserModel from "@models/user.Model";
+import jwt  from "jsonwebtoken";
 export const registerWithEmailPasswordController =async (req: Request, res: Response,next:NextFunction) => {
     try {
         const { username, password } = req.body;
-    const createUser =await UserModel.create({username,password})
-    // jwt.sign({ userId: createUser._id }, String(process.env.JWT_SECRET), (err:any, token:string) => {
-    //     if(err) throw err
-    // })
-    Result(res,"");
+        const createUser =await UserModel.create({username,password});
+        jwt.sign({ userId: createUser._id }, String(process.env.JWT_SECRET), (err:any, token:any) => {
+            if(err) throw err;
+            return res.cookie("token",token).status(201).json("ok");
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
 
