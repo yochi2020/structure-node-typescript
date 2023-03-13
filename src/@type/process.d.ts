a556a1feb2ffc9ddfcef5cd488e492655842571b
Chanaphat <chanaphat@iapp.co.th>
2023-03-13 08:36:37 +0700

    .

--- src/@type/process.d.ts
+++ src/@type/process.d.ts
@@ -3,10 +3,12 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT?: string;
     SALT?: string;
+    CLIENT_URL?: string;
     JWT_ACCESS_SECRET?: string;
-    JWT_REFRESH_TOKEN_SECRET?:string
-    JWT_RESET_PASSWORD?:string
-    JWT_ACCOUNT_ACTIVATION?:string
+    JWT_REFRESH_TOKEN_SECRET?: string;
+    JWT_RESET_PASSWORD?: string;
+    JWT_ACCOUNT_ACTIVATION?: string;
+    EMAIL_FROM?: string;
   }
 }
 
